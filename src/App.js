a944b6cf6d57eada2aee9e248b56493088ba679e
Robbie Jaeger <jaeger.rob@gmail.com>
2021-01-12 11:37:52 -0700

    Change h1 title to match purpose of app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Auth Practice</h1>
+        <h1>Auth Practice - Show Your Wins</h1>
       </header>
     </div>
   );
